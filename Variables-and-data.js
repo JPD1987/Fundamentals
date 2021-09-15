207a2c588750fd3adee07139eabbf64fe64f73be
Justin Desoto <justin.desoto@yahoo.com>
2021-09-15 14:52:45 -0600

    phase 2 variables and types

---
+++ Variables-and-data.js
@@ -0,0 +1,14 @@
+/*
+* Variables, Data types, and typing
+*/
+let wordData;
+
+wordData= "Word list"
+
+console.log(wordData)
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+
---
+++ Variables-and-data2.js
@@ -0,0 +1,12 @@
+/*
+ * Variables, Data types, and typing
+ */
+let obj = {
+    key1: "Value 1",
+    key2: 3,
+    obj2: {
+        obj2Key1 : "value of inner object",
+    },
+};
+
+console.log(obj.obj2.obj2Key1);
